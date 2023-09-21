c0732077e452944928100edab650ecf269aa4f84
dav-co <davidedamiano.colella@studenti.unisalento.it>
2023-09-21 17:11:57 +0200

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='euFAIR-alpha',
+    version='alpha',
+    packages=['log', 'test', 'test.metadata', 'utility', 'reporter', 'settings', 'ext_point', 'db_manager',
+              'from_portal', 'cloud_writer', 'data_evaluator', 'data_evaluator.csv_test', 'data_evaluator.non_specific',
+              'data_retriever', 'metadata_evaluator'],
+    url='',
+    license='',
+    author='dav-co',
+    author_email='davidedamiano.colella@studenti.unisalento.it',
+    description=''
+)
